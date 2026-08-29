4cd752dd8fcc72cff169d35440e7d72cd1a2005f
jyoti ranjan sahoo <zoro50964@gmail.com>
2026-08-30 00:33:35 +0530

    Modify QueryProvider to enhance query client settings
    Updated query client configuration with new options for stale time and garbage collection.

--- frontend/providers/QueryProvider.js
+++ frontend/providers/QueryProvider.js
@@ -1,19 +1,31 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
 import { useState } from "react";
 
-export default function QueryProvider({ children }) {
+export default function QueryProvider({
+  children,
+}) {
   const [queryClient] = useState(
     () =>
       new QueryClient({
         defaultOptions: {
           queries: {
+            staleTime: 2 * 60 * 1000,
+
+            gcTime: 10 * 60 * 1000,
+
             retry: 1,
             refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            refetchOnReconnect: false,
           },
         },
-      })
+      }),
   );
 
   return (
