1228c4830dbe26626e84171fe0ad80459a95b4f7
jyoti ranjan sahoo <zoro50964@gmail.com>
2026-08-30 00:44:11 +0530

    Refactor HeroBackground component with loading and comments
    Updated image components to include loading attribute and added comments for clarity.

--- frontend/app/components/home/HeroBackground.js
+++ frontend/app/components/home/HeroBackground.js
@@ -6,37 +6,41 @@ import Floating from "@/app/components/animation/Floating";
 export default function HeroBackground() {
   return (
     <>
-      <Floating y={20} duration={7}>
+      {/* Decorative background blob */}
+      <Floating y={12} duration={8}>
         <Image
           src="/blob/blob.svg"
-          alt="blob1"
+          alt=""
           width={350}
           height={350}
-          priority
+          loading="lazy"
           aria-hidden="true"
           className="pointer-events-none absolute -left-24 top-24 -z-10 opacity-40"
         />
       </Floating>
 
-      <Floating y={15} duration={9}>
+      {/* Decorative background blob */}
+      <Floating y={10} duration={10}>
         <Image
           src="/blob/blob2.svg"
-          alt="blob 2"
+          alt=""
           width={450}
           height={450}
+          loading="lazy"
           aria-hidden="true"
           className="pointer-events-none absolute -right-32 bottom-0 -z-10 opacity-30"
         />
       </Floating>
 
-      <Floating y={10} duration={11}>
+      {/* Decorative background blob */}
+      <Floating y={8} duration={12}>
         <Image
           src="/blob/blob3.svg"
-          alt="blob 3"
+          alt=""
           width={250}
           height={250}
+          loading="lazy"
           aria-hidden="true"
-          priority
           className="pointer-events-none absolute right-1/3 top-12 -z-10 opacity-25"
         />
       </Floating>
